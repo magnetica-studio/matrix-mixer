c9de6a8e6ffda86938e8ecdb3f7c1c9e576cd5e8
szk2s <suzukisjss@gmail.com>
2021-12-02 17:01:38 +0900

    remove unused codes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { ElementaryPluginRenderer as core } from "@nick-thompson/elementary";
 
 core.on("load", (e) => {
@@ -20,8 +19,3 @@ core.on("load", (e) => {
 });
 
 core.initialize();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
