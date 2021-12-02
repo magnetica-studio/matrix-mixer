7c418214e9b6b55402c3d215021c02fe05249fb8
szk2s <suzukisjss@gmail.com>
2021-12-02 13:39:46 +0900

    introduce practical state structure

--- src/App.js
+++ src/App.js
@@ -2,33 +2,48 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import {
   el,
-  ElementaryPluginRenderer as core,
+  ElementaryPluginRenderer as core
 } from "@nick-thompson/elementary";
 
-function App({ loadEvent }) {
-  let [patchBay, setPatchBay] = useState([
-    [1, 0],
-    [1, 0],
-  ]);
+let outputs = [
+  {
+    name: "L",
+    receive: [
+      {
+        gain: 1.0,
+        mute: false,
+      },
+      {
+        gain: 0.0,
+        mute: false,
+      },
+    ],
+  },
+  {
+    name: "R",
+    receive: [
+      {
+        gain: 0.0,
+        mute: false,
+      },
+      {
+        gain: 1.0,
+        mute: false,
+      },
+    ],
+  },
+];
 
-  let toggle = (i, j, value) => {
-    // Duplicate our state to avoid in-place mutation
-    const newBay = patchBay.map((x) => [...x]);
-
-    // Update and write the new state
-    newBay[i][j] = 1 - newBay[i][j];
-    setPatchBay(newBay);
-  };
-
+const App = ({ loadEvent }) => {
   useEffect(() => {
     core.render(
-      ...patchBay.map((row, i) => {
+      ...outputs.map(output => {
         return el.add(
-          ...row.map((val, j) => {
-            let key = `pb${i}:${j}`;
+          ...output.receive.map(input => input.gain * (input.mute ? 0 : 1)).map((gain, inputIndex) => {
+            let key = `pb${output.name}:${inputIndex}`;
             return el.mul(
-              el.sm(el.const({ key, value: 0.2 * val })),
-              el.in({ channel: j })
+              el.sm(el.const({ key, value: gain })),
+              el.in({ channel: inputIndex })
             );
           })
         );
@@ -37,42 +52,25 @@ function App({ loadEvent }) {
   });
 
   return (
-    <div className="px-8 py-4">
-      <h1 className="text-gray-500">Patch Bay Demo</h1>
-      {loadEvent.numInputChannels}
-      <table className="border-collapse border border-gray-400">
+    <table>
+      <tr>
+        {outputs[0].receive.map((input, index) => (
+          <th>input {index}</th>
+        ))}
+      </tr>
+      {outputs.map((output) => (
         <tr>
-          <td className="py-4 text-gray-500 font-semibold font-mono text-xs"></td>
-          <td className="py-4 text-gray-500 font-semibold font-mono text-xs">
-            <div className="transform -rotate-90">Input 0</div>
-          </td>
-          <td className="py-4 text-gray-500 font-semibold font-mono text-xs">
-            <div className="transform -rotate-90">Input 1</div>
-          </td>
+          {output.receive.map((input) => (
+            <td>
+              <div>gain: {input.gain}</div>
+              <div>mute: {input.mute}</div>
+            </td>
+          ))}
+          <td>{output.name}</td>
         </tr>
-        {patchBay.map(function (row, i) {
-          return (
-            <tr key={i}>
-              <td className="px-2 text-gray-500 font-semibold font-mono text-xs">
-                Output {i}
-              </td>
-              {row.map(function (cell, j) {
-                return (
-                  <td key={j} className="p-2 border border-gray-400">
-                    <input
-                      type="checkbox"
-                      checked={cell === 1}
-                      onInput={(e) => toggle(i, j, e.target.value)}
-                    />
-                  </td>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </table>
-    </div>
+      ))}
+    </table>
   );
-}
+};
 
 export default App;
